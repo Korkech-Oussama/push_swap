29254c588318e1680c53569bbc597066830bd327
Oussama Korkech <okorkech@e1r4p11.1337.ma>
2026-01-11 20:27:23 +0100

    update

--- moves/push.c
+++ moves/push.c
@@ -10,3 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../push_swap.h"
+
+void  add_top(t_stack **dest, t_stack *new_node)
+{
+    t_stack *tail;
+    t_stack *first;
+    
+    first = *dest;
+    tail = first->prev;
+    new_node->next = first;
+    new_node->prev = tail;
+    first->prev = new_node;
+    tail->next = new_node;
+    *dest = new_node;
+}
+
+void  push(t_stack **dest, t_stack **src)
+{
+  t_stack *second;
+  t_stack *tail_src;
+
+  if (!*src)
+    return ;
+  if (!*dest)
+  {
+    second = (*src)->next;
+    tail_src = (*src)->prev;
+    (*src)->next = *src;
+    (*src)->prev = *src;
+    *dest = *src;
+    *dest = *src;
+    second->prev = tail_src;
+    tail_src->next = second;
+    *src = second;
+  }
+  else
+  {
+      second = (*src)->next;
+      tail_src = (*src)->prev;
+      add_top(dest,*src);
+      second->prev = tail_src;
+      tail_src->next = second;
+      *src = second;
+  }
+}
--- moves/rotate.c
+++ moves/rotate.c
@@ -14,5 +14,26 @@
 
 void	rotate(t_stack	**head)
 {
-	*head = (*head)->prev;
+	if (!*head || !(*head)->next || *head == !(*head)->next)
+		return ;
+	*head = (*head)->next;
+}
+
+void	ra(t_stack **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack **b)
+{
+	rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
 }
